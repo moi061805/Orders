e5f8982e1b541dfe1a7a1bd9a5c7407fb339957c
moi061805 <84187816+moi061805@users.noreply.github.com>
2021-06-04 13:50:04 -0400

    Update Main.java

--- src/Store/Main.java
+++ src/Store/Main.java
@@ -2,7 +2,7 @@ package Store;
 
 
 public class Main {
-	
+//This class creates the program
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MasterOrder obj = new MasterOrder();

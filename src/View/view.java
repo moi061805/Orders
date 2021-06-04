ce6c2e886f63373cf4e72cb9d313775056b336cc
moi061805 <84187816+moi061805@users.noreply.github.com>
2021-06-04 13:36:09 -0400

    Update view.java

--- src/View/view.java
+++ src/View/view.java
@@ -6,10 +6,10 @@ import Store.Order;
 import cont.JOP;
 
 public class view {
-	
+//This class displays the program
 	private Order _ord;
 	private MasterOrder _mas;
-	
+// This method is creates the store
 	public String generateStore() {
 		
 		String msg = "What do you want to do";

e5395efecead7fabdec64c2baa0946d9bac91e98
moi061805 <84187816+moi061805@users.noreply.github.com>
2021-06-04 14:06:09 -0400

    Update MasterOrder.java

--- src/Store/MasterOrder.java
+++ src/Store/MasterOrder.java
@@ -5,17 +5,18 @@ import java.util.ArrayList;
 import cont.JOP;
 
 public class MasterOrder {
+	//This class creates a Master order
 	private ArrayList<Order> orders;
 	
 	public MasterOrder() {
 		orders = new ArrayList<Order>();
 		
 	}
-	
+	//This method adds an order
 	public void addOrder(Order o) {
 		orders.add(o);
 	}
-	
+	//This method prints all the orders
 	public void printOrders() {
 		String orderStr = "";
 		for(Order o : orders) {
@@ -23,7 +24,7 @@ public class MasterOrder {
 		}
 		JOP.msg(orderStr);
 	}
-	
+	//This method prints the order of an specific buyer
 	public void printOrders(String buyer) {
 		String orderStr = "";
 		for(Order o : orders) {
@@ -33,7 +34,7 @@ public class MasterOrder {
 		}
 		JOP.msg(orderStr);
 	}
-	
+	//This method clears a product from the orders
 	public void clearProduct(String product) {
 		for(int i = 0; i < orders.size(); i++) {
 			if(orders.get(i).getProduct().equals(product)) {
@@ -42,7 +43,7 @@ public class MasterOrder {
 			}
 		}
 	}
-	
+	//This method clears a buyer from the orders
 	public void clearBuyer(String buyer) {
 		for(int i = 0; i < orders.size(); i++) {
 			if(orders.get(i).getBuyer().equals(buyer)) {
@@ -51,7 +52,7 @@ public class MasterOrder {
 			}
 		}
 	}
-	
+	//This method removes a product and a buyer from the orders
 	public void remove(String product, String buyer) {
 		for(int i = 0; i < orders.size(); i++) {
 			if(orders.get(i).getBuyer().equals(buyer) && orders.get(i).getProduct().equals(product)) {
